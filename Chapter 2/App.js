3d99b2ee4236b93bcf5429f003a58a3a9c6960dc
Amitanshu <amitanshuabhishek17@gmail.com>
2023-01-03 23:48:25 +0530

    Add Chapter 2

---
+++ Chapter 2/App.js
@@ -0,0 +1,16 @@
+const heading1 = React.createElement("h1", {
+    id: "title"
+    } , "Heading 12"); 
+    const heading2 = React.createElement("h2", {
+    id: "title"
+    } , "Heading 2222 "); 
+    const container = React.createElement("div", {
+    id: "container"
+    } , [heading1,heading2]); 
+    
+    //console.log(heading); Every object in the react is an react element
+    
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    
+    //passing a react element inside a root.  
+    root.render(container);
