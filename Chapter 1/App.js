43efb3daf1e22d9b13f067a334f043412106b785
Amitanshu <amitanshuabhishek17@gmail.com>
2023-01-03 16:32:07 +0530

    Add all the files

--- Chapter 1/App.js
+++ Chapter 1/App.js
@@ -1,9 +1,9 @@
 const heading1 = React.createElement("h1", {
     id: "title"
-    } , "Heading 1"); 
+    } , "Heading 12"); 
     const heading2 = React.createElement("h2", {
     id: "title"
-    } , "Heading 2 "); 
+    } , "Heading 2222 "); 
     const container = React.createElement("div", {
     id: "container"
     } , [heading1,heading2]); 
